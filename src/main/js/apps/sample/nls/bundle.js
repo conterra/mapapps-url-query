2f6e56efc216adf1ffc673a49575f17702353fe4
ChrissW-R1 <contact@ChrissWR1.me>
2017-08-18 12:34:16 +0200

    added information window to show example links in sample app

---
+++ src/main/js/apps/sample/nls/bundle.js
@@ -0,0 +1,13 @@
+define({
+    root: {
+        contentviewer: {
+            urlinfo: {
+                title: "Examples",
+                content: "<p>The following links are examples to start queries with the URL Query bundle:</p>",
+                countryText: "All countries, which ISO-code starts with 'F'",
+                waterText: "All 'good' water quality on 2004 in Belgium"
+            }
+        }
+    },
+    "de": true
+});
---
+++ src/main/js/apps/sample/nls/de/bundle.js
@@ -0,0 +1,10 @@
+define({
+    contentviewer: {
+        urlinfo: {
+            title: "Beispiele",
+            content: "<p>Die folgenden Links sind Beispiele zum Starten von Abfragen mit dem URL Query bundle:</p>",
+            countryText: "Alle Länder, welche ISO-Codes mit 'F' beginnen",
+            waterText: "Alle 'guten' Wasserqualitäten in Belgium (2004)"
+        }
+    }
+});
