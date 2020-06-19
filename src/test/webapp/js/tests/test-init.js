30cedf2ad02ac5a8b69fb4b2144502222efd69eb
Christian Weckermann <christian.weckermann@hs-bochum.de>
2020-06-19 10:36:19 +0200

    *** broken *** merged some fresh version 4 files

--- src/test/webapp/js/tests/test-init.js
+++ src/test/webapp/js/tests/test-init.js
@@ -5,12 +5,21 @@ testConfig({
                 // register all self hosted packages
                 "*"
             ]
-        },
-        {
+        }
+        //uncomment, if project runs in remote mode
+        ,{
             root: "@@mapapps.remote.base@@/resources/jsregistry/root",
             packages: [
-                "apprt@3.x"
+                "apprt-polyfill",
+                "apprt@4.x",
+                "apprt-vue@4.x",
+                "vuetify@~0.14.7",
+                "esri@4.x"
             ]
         }
+    ],
+    // ensure babel polyfill is loaded during test execution
+    deps: [
+        "apprt-polyfill"
     ]
 });
