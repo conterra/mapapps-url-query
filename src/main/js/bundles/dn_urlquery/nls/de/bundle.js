2cc67cfb2a4fbcf232cec63e2344fa7cbf2e6c16
Christian Weckermann <christian.weckermann@hs-bochum.de>
2020-06-19 13:17:11 +0200

    converted nls files

--- src/main/js/bundles/dn_urlquery/nls/de/bundle.js
+++ src/main/js/bundles/dn_urlquery/nls/de/bundle.js
@@ -1,13 +1,8 @@
-define(
-    /*
-     * COPYRIGHT 2012 con terra GmbH Germany
-     */
-    ({
-        bundleName: "URL Query",
-        bundleDescription: "Dieses Bundle erm\u00F6glicht die Abfrage von Features \u00FCber die URL.",
-        notifications: {
-            totalError: "Ein Fehler ist aufgetreten, bei der Abfrage der Ergebnisgr\u00F6\u00DFe vom Store ${store}!",
-            tooManyFeatures: "Es wurden zu viele Features vom Store ${store} abgefragt!"
-        }
-    })
-);
+module.exports = {
+	bundleName: "URL Query",
+	bundleDescription: "Dieses Bundle erm\u00F6glicht die Abfrage von Features \u00FCber die URL.",
+	notifications: {
+		totalError: "Ein Fehler ist aufgetreten, bei der Abfrage der Ergebnisgr\u00F6\u00DFe vom Store ${store}!",
+		tooManyFeatures: "Es wurden zu viele Features vom Store ${store} abgefragt!"
+	}
+};
